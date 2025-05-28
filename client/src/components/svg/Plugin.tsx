049512f9017245b473057767fbf3aaa2d14ed9ad
mycroft123 <mycroft55@hotmail.com>
2025-05-28 11:17:16 -0400

    logo full

--- client/src/components/svg/Plugin.tsx
+++ client/src/components/svg/Plugin.tsx
@@ -4,7 +4,11 @@ export default function Plugin({ className = '', ...props }) {
   return (
     <img 
       src="/assets/logo.svg"
-      className={cn('h-4 w-4', className)}
+      className={cn('h-full w-full', className)} // Changed from 'h-4 w-4' to 'h-full w-full'
+      style={{ 
+        objectFit: 'cover',  // Makes the image fill the container
+        scale: '1.5'         // Makes it 50% bigger to eliminate blue background
+      }}
       alt="DeFacts"
       {...props}
     />
